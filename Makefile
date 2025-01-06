3f94151c921881505b02269dd7b5b0f2f97a3968
jake-t-dev <jake.turner@dailypay.com>
2025-01-06 19:15:48 +0000

    port fixes

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ docker-build:
 	@docker build -t shortenit-api:latest . 
 
 docker-run: docker-build
-	@docker run -d -p 3000:3000 shortenit-api:latest
+	@docker run -d -p 5000:5000 shortenit-api:latest
 
 run: 
 	@npm run dev
--- internal/app.js
+++ internal/app.js
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.listen(8080, () => {
+    console.log('Server is running on port 8080');
 });
